c18d73a7d7918d69bc024bb27a1f7c80a25faffb
Guilherme Sernajoto <guilhermesernajoto@hotmail.com>
2018-10-22 15:14:02 -0300

    Ativ 10 - Adicionando Derby DB

--- Ativ9/prjCadastroAluno/src/main/java/managedBeans/AlunoMB.java
+++ Ativ9/prjCadastroAluno/src/main/java/managedBeans/AlunoMB.java
@@ -1,30 +1,40 @@
 package managedBeans;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import entidades.Aluno;
 
 @ManagedBean
 @SessionScoped
 public class AlunoMB {
-	private List<Aluno> listaAlunos = new ArrayList<Aluno>();
 	private Aluno aluno = new Aluno();
-	
+	private EntityManagerFactory emf;
+
+	public AlunoMB() {
+		emf = Persistence.createEntityManagerFactory("prjCadastroAlunoDB");
+	}
+
 	public void salvar() {
-		listaAlunos.add(aluno);
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(aluno);
+		em.getTransaction().commit();
+		em.close();
 		aluno = new Aluno();
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Aluno> getListaAlunos() {
+		List<Aluno> listaAlunos;
+		EntityManager em = emf.createEntityManager();
+		listaAlunos = em.createQuery("Select a from Aluno a").getResultList();
 		return listaAlunos;
-	}
-
-	public void setListaAlunos(List<Aluno> listaAlunos) {
-		this.listaAlunos = listaAlunos;
 	}
 
 	public Aluno getAluno() {
